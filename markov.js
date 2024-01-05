7b24464b5326d90e2a57b4f5eeb4cfab1a9c4273
tobias-homewood <toby_homewood@hotmail.co.uk>
2024-01-05 18:35:25 +0000

    maketext function

--- markov.js
+++ markov.js
@@ -22,13 +22,13 @@ class MarkovMachine {
     this.words.forEach(target => {
       if (this.chains[target]) return;
 
-      const chain = new Set();
+      const chain = [];
       this.words.forEach((word, index) => {
         if (target === word) {
-          chain.add(this.words[index + 1] || null);
+          chain.push(this.words[index + 1] || null);
         }
       });
-      this.chains[target] = [...chain];
+      this.chains[target] = chain;
     });
   }
 
@@ -36,6 +36,20 @@ class MarkovMachine {
   /** return random text from chains */
 
   makeText(numWords = 100) {
-    // TODO
+    const text = [];
+    const words = Object.keys(this.chains);
+
+    // starting word
+    let word = words[Math.floor(Math.random() * words.length)];
+
+    while (text.length < numWords && word !== null) {
+      text.push(word);
+
+      const suggestions = this.chains[word];
+      word = suggestions[Math.floor(Math.random() * suggestions.length)];
+    }
+
+    return text.join(" ");
   }
 }
+exports.MarkovMachine = MarkovMachine;
